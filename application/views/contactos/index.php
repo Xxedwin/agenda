5749c1a77de71a43809acaf2487892237bebc841
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2017-10-19 02:02:49 -0500

    Soluciono el problema de la validación con html5 y uso de ajax

--- application/views/contactos/index.php
+++ application/views/contactos/index.php
@@ -4,18 +4,31 @@
 	<div class="row">
 		<div class="col-md-12">
 			<center>
-				<?php
-					foreach ($query as $roww) {
-						/*echo $row ->Id . "<br />";*/
-						echo $roww -> Nombre . "<br />";
-						echo $roww -> idpais . "<br />";
-						echo $roww -> idregion . "<br />";
-						echo $roww -> Telefono . "<br />";
-						echo $roww -> mensaje . "<br />";						
-						echo "<br />";
-					}
-				?>
-			</center>
+<table id="id" class="table table-borderer">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>idPais</th>
+            <th>idregion</th>
+            <th>Telefono</th>
+            <th>Mensaje</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach ($query as $roww) {
+					/*echo $row ->Id . "<br />";*/
+                    echo "<tr>";
+                    echo "<td>".$roww -> Nombre   . "</td>";
+                    echo "<td>".$roww -> idpais   . "</td>";
+					echo "<td>".$roww -> idregion . "</td>";
+					echo "<td>".$roww -> Telefono . "</td>";
+					echo "<td>".$roww -> mensaje  . "</td>";						
+                    echo "</tr>";
+			}
+        ?>
+    </tbody>
+</table>
 			
 		</div>
 	</div>
